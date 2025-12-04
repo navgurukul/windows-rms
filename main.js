cf12670441aa20ac313e540e0aebc86a4552f467
unknown <mukulgautam18112000@gmail.com>
2025-12-04 13:18:13 +0530

    Added selective wallpaper changes fucntionality

--- main.js
+++ main.js
@@ -140,7 +140,7 @@ async function registerDeviceToServer() {
 async function fetchAndSetWallpaper() {
   try {
     console.log('Fetching wallpaper URL...');
-    const fetchWallpaper = await axios.get(`${config.BACKEND_BASE_URL}/api/wallpaper`);
+    const fetchWallpaper = await axios.get(`${config.BACKEND_BASE_URL}/api/wallpaper/${await metricService.getSerialNumber()}`);
     const url = fetchWallpaper.data.wallpaper;
     console.log('Wallpaper URL fetched:', url);
 
